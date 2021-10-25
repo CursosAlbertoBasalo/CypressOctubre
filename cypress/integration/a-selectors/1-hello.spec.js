7dc7db0f7e37fd810a1b8668876b957f8a188aa1
Alberto Basalo <albertobasalo@hotmail.com>
2021-10-25 17:47:42 +0200

    test: 1 hola cypress

---
+++ cypress/integration/a-selectors/1-hello.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="cypress" />
+
+// describir lo que pasa , entre comillas
+// funciones que ejecutan
+
+describe('My first test', () => {
+  it('should visit the initial page', () => {
+    // act assert
+    cy.visit('https://angularbuilders.github.io/angular-budget/')
+    cy.contains('Budget')
+    cy.get('pre').should('contain', 'Budget')
+  })
+});
+
+
+// Arrange
+// Act
+// Assert
