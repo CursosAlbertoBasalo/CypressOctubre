71d0e9c8db94aceca8f4a811382207c7713752a9
Alberto Basalo <albertobasalo@hotmail.com>
2021-10-27 18:50:07 +0200

    test: 7 api

---
+++ cypress/integration/c-interceptors/7-api.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('WHEN: make a get _projects_ request', () => {
+  it('should get an array with 1 or more items', () => {
+    cy.request(Cypress.env('apiUrl') + 'projects/').then(response => {
+      expect(response.body.data).to.have.length.above(1);
+    })
+  })
+})
+
+describe('WHEN: make a delete reques', () => {
+  it('THEN: should get either a _not authenticated_ a _deleted_ or _not found_ response', () => {
+    cy.request({
+      method: 'DELETE',
+      url: Cypress.env('apiUrl') + 'projects/' + 'conquer_mars',
+      body: {},
+      failOnStatusCode: false,
+    }).then(response => {
+      expect(response.status).to.be.oneOf([401, 403, 200, 204, 404]);
+    })
+  })
+})
