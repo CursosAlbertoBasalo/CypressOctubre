bad48912b1ba37a4259045ea4f792844ba16aeab
Alberto Basalo <albertobasalo@hotmail.com>
2021-10-27 18:24:26 +0200

    docs: 6 interceptors

--- cypress/integration/c-interceptors/6-interceptors.spec.js
+++ cypress/integration/c-interceptors/6-interceptors.spec.js
@@ -3,17 +3,22 @@ import { ProjectsAddPage } from '../../support/pages/projects-add.page';
 
 describe('GIVEN the dashboard on the _home_ page', () => {
   beforeEach(() => {
-    cy.intercept(Cypress.env('apiUrl') + 'projects/', {
-      "fixture": "projects",
-      "delay": 500,
-    }).as('getProjects');
+    // Arrange
+    // intercepts the requests and responds with predefined data
+    cy.intercept(
+      'GET', // http method
+      Cypress.env('apiUrl') + 'projects', // url
+      { fixture: 'projects' } // static response
+    ).as('getProjects');
     cy.visit('/');
-  })
+  });
   context('WHEN get projects loaded from the API', () => {
     beforeEach(() => {
+      // Act
       cy.waitFor('@getProjects');
     });
     it('THEN should display those projects', () => {
+      // Assert
       cy.get('ab-projects ab-card').should('have.length', 2);
     });
   });
@@ -21,26 +26,33 @@ describe('GIVEN the dashboard on the _home_ page', () => {
 
 describe('GIVEN the _Add new project_ page, connected to a teapot', () => {
   const projectsAddPage = new ProjectsAddPage();
-  before(() => {
+  beforeEach(() => {
+    // Arrange
     projectsAddPage.visit();
+    // intercept of post request and responds with an error
     cy.intercept(
-      'POST',
-      Cypress.env('apiUrl') + 'projects/',
+      'POST', // method
+      Cypress.env('apiUrl') + 'projects', // url
       {
-        "body": { data: {} },
-        "statusCode": 418
-      }
-    ).as('postProject');
-  })
+        statusCode: 418, // forced error response (teapot)
+        body: { data: {} }, // empty response
+      },
+    ).as('postProject'); // alias for the request
+  });
   context('WHEN fill the form and submit to the teapot', () => {
-    before(() => {
+    beforeEach(() => {
+      // Act
       projectsAddPage.fillCorrectly();
       projectsAddPage.submitForm();
     });
-    it('THEN data should be posted', () => {
-      cy.wait('@postProject').then(intercepted => {
-        expect(intercepted.request.body.name).to.be.eq('Collapse the global traffic')
-      })
+    it('THEN data should be posted ', () => {
+      // Assert
+      cy.wait('@postProject') // wait for the request to be done and assert with a function
+        .then(intercepted => {
+          // check that the project was posted
+          expect(intercepted.request.body.name) // assert using the intercepted request
+            .to.deep.eq('Collapse the global traffic'); // check the request body
+        });
     });
   });
 });
--- cypress/integration/c-interceptors/6e-interceptors.spec.js
+++ cypress/integration/c-interceptors/6e-interceptors.spec.js
@@ -1,3 +1,4 @@
+// 📋 To do labs:
 // GIVEN the dashboard on the _home_ page'
 // WHEN get no projects come from server
 // THEN show no data yet...
